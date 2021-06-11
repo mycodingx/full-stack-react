432465459a3cfcc0554a275f7eee942336f95d4b
Atul Kumar <mycodingx@gmail.com>
2021-06-11 10:40:25 +0530

    adding home page with styled components

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
 
-export const index = () => {
-  return <div></div>;
-};
+const PageContainer = styled.div`
+  ${tw`
+   flex
+   flex-col
+   w-full
+   h-full
+   items-center
+   overflow-x-hidden
+`}
+`;
+
+export function HomePage() {
+  return <PageContainer>Hello World..!!</PageContainer>;
+}
