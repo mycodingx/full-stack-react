524b79cbb891e4ab43be1e2f0a31333042a73c2f
Atul Kumar <mycodingx@gmail.com>
2021-06-11 12:33:24 +0530

    setting up burger menu with menuStyles done

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive/index";
+import { slide as Menu } from "react-burger-menu";
+import menuStyles from "./menuStyles";
 
 const ListContainer = styled.ul`
   ${tw`
@@ -9,9 +13,9 @@ list-none
 `};
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
-text-xs
+text-sm
 md:text-base
 text-black
 font-medium
@@ -20,11 +24,44 @@ md:mr-5
 cursor-pointer
 transition
 duration-300
+ease-in-out
 hover:text-gray-700
 `};
+
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+      text-white
+      text-xl
+      mb-3
+      focus:text-white
+    `};
+    `};
 `;
 
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Contact Us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
   return (
     <ListContainer>
       <NavItem>
